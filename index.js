23a84363952b6eaffd02c6c2f5d41e704ac085c5
sumit <97390822+Sumit9953@users.noreply.github.com>
2023-08-25 20:22:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ function moveBackground(event){
     const shapes = document.querySelectorAll(".shape");
     const x = event.clientX * scaleFcator;
     const y = event.clientY * scaleFcator;
-     
+
+    while(true){
     for (let i = 0; i < shapes.length; i++) {
         const isOdd = i % 2 === 0;
         const boolInt = isOdd ? -1 : 1; 
         shapes[i].style.transform = `translate(${x * boolInt}px,${y * boolInt}px)`;
         
+    }
     }
 }
 
